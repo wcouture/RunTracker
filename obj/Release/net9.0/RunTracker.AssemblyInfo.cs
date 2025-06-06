ceb34d3e3f969cc7508e3ec32e44583751b24582
William Couture <wcouture17@gmail.com>
2025-06-05 22:08:49 -0400

    publishs blazor app

--- obj/Release/net9.0/RunTracker.AssemblyInfo.cs
+++ obj/Release/net9.0/RunTracker.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("RunTracker")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e96903831163bdb4554a30aedd03841983a29235")]
 [assembly: System.Reflection.AssemblyProductAttribute("RunTracker")]
